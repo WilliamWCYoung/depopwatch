6cffb6e37791c5ed099af942f4683f629d9265f7
nedshnebly <nedshnebly@hotmail.co.uk>
2016-08-16 16:51:58 +0100

    Docstrings end with a period.
    "The docstring is a phrase ending in a period."

--- depopwatch/depopwatch.py
+++ depopwatch/depopwatch.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 class Listener:
     def __init__(self,username):
-        """Initalize a depop watch instance
+        """Initalize a depop watch instance.
 
             Argument:
             username -- username of the account you want to watch on Depop
@@ -25,7 +25,7 @@ class Listener:
         self.username = username
 
     def _get_current_items(self):
-        """Get the current items listed on the given depop account"""
+        """Get the current items listed on the given depop account."""
        
         r = requests.get("https://www.depop.com/en/"+self.username)
 
@@ -37,7 +37,7 @@ class Listener:
         return []
 
     def search_account(self):
-        """Seach the given depop account, returning any new items
+        """Seach the given depop account, returning any new items.
 
             Example:
             >>> listener.search_account()
